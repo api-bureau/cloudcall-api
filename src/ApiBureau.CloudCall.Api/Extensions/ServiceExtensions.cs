68439778c50942eaa4c2bac0051bb5cfe320dfb8
Vaclav Elias <vaclavelias@gmail.com>
2022-05-08 23:21:14 +0100

    Usings removed

--- src/ApiBureau.CloudCall.Api/Extensions/ServiceExtensions.cs
+++ src/ApiBureau.CloudCall.Api/Extensions/ServiceExtensions.cs
@@ -1,8 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
-using System;
-using System.Net.Http;
 
 namespace ApiBureau.CloudCall.Api.Extensions
 {
