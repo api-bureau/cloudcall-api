65127158fce08a34d9390fd2864d97b034053dfb
Vaclav Elias <vaclavelias@gmail.com>
2022-08-07 23:57:16 +0100

    feat: Example updated

--- src/ApiBureau.CloudCall.Api.Console/Services/DataService.cs
+++ src/ApiBureau.CloudCall.Api.Console/Services/DataService.cs
@@ -20,8 +20,9 @@ namespace ApiBureau.CloudCall.Api.Console.Services
 
             var accounts = await _client.Accounts.GetAsync();
             var items = await _client.Calls.GetAsync(DateTime.Now.AddDays(-2), DateTime.Now);
+            var items2 = await _client.Calls.GetAsync(DateTime.Now.AddDays(-2), DateTime.Now, 60 * 24);
 
-            _logger.LogInformation("Items: {0}", items.Count());
+            _logger.LogInformation("Accounts: {0}, Items: {1}, Items: {1}", accounts.Count, items.Count, items2.Count);
 
             //var dto = await _client.Skills.GetStatusAsync();
 
