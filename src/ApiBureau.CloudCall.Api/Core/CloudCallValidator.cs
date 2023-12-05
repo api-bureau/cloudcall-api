8c8248d9b9b939f95a641c03232b9a6ea90e4e7c
Vaclav Elias <vaclavelias@gmail.com>
2023-12-05 10:39:54 +0000

    refactor: Validator improvements

--- src/ApiBureau.CloudCall.Api/Core/CloudCallValidator.cs
+++ src/ApiBureau.CloudCall.Api/Core/CloudCallValidator.cs
@@ -1,17 +1,30 @@
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace ApiBureau.CloudCall.Api.Core;
 
+/// <summary>
+/// Provides validation methods for CloudCall settings and access tokens.
+/// This static class contains methods to ensure that necessary configuration settings
+/// and access tokens are present and valid for CloudCall operations.
+/// </summary>
 public static class CloudCallValidator
 {
-    private const string MissingSettings = "Settings are missing";
-    private const string MissingBaseUrl = "BaseUrl in settings is missing or empty";
-    private const string MissingLoginUrl = "LoginUrl in settings is missing or empty";
-    private const string MissingUserName = "UserName in settings is missing or empty";
+    private const string MissingSettings = "Settings are missing in the appsettings.json or secret.json";
+    private const string MissingBaseUrl = "Base Url in settings is missing or empty";
+    private const string MissingLoginUrl = "Login Url in settings is missing or empty";
+    private const string MissingUserName = "Username in settings is missing or empty";
     private const string MissingPassword = "Password in settings is missing or empty";
-    private const string MissingLicenceKey = "LicenseKey in settings is missing or empty";
+    private const string MissingLicenceKey = "License key in settings is missing or empty";
     private const string MissingAccessToken = "Access token is missing or invalid";
 
+    /// <summary>
+    /// Validates the provided CloudCall settings.
+    /// </summary>
+    /// <param name="settings">The CloudCallSettings object to validate.</param>
+    /// <param name="logger">The logger to log any validation errors.</param>
+    /// <exception cref="ArgumentNullException">Thrown if settings are null.</exception>
+    /// <exception cref="ArgumentException">Thrown if any of the settings properties are invalid.</exception>
     public static void ValidateSettings(CloudCallSettings settings, ILogger logger)
     {
         if (settings is null)
@@ -20,49 +33,53 @@ public static class CloudCallValidator
             throw new ArgumentNullException(nameof(settings), MissingSettings);
         }
 
-        var errors = new List<string>();
+        var errors = new StringBuilder();
 
         if (string.IsNullOrWhiteSpace(settings.BaseUrl))
         {
-            errors.Add(MissingBaseUrl);
+            errors.AppendLine(MissingBaseUrl);
         }
 
         if (string.IsNullOrWhiteSpace(settings.LoginUrl))
         {
-            errors.Add(MissingLoginUrl);
+            errors.AppendLine(MissingLoginUrl);
         }
 
         if (string.IsNullOrWhiteSpace(settings.UserName))
         {
-            errors.Add(MissingUserName);
+            errors.AppendLine(MissingUserName);
         }
 
         if (string.IsNullOrWhiteSpace(settings.Password))
         {
-            errors.Add(MissingPassword);
+            errors.AppendLine(MissingPassword);
         }
 
         if (string.IsNullOrWhiteSpace(settings.LicenseKey))
         {
-            errors.Add(MissingLicenceKey);
+            errors.AppendLine(MissingLicenceKey);
         }
 
-        if (errors.Any())
+        if (errors.Length > 0)
         {
-            var errorMessage = string.Join("; ", errors);
+            var errorMessage = errors.ToString().TrimEnd();
 
-            logger.LogError("Settings errors: {errors}", errorMessage);
-
+            logger.LogError("Settings validation errors: {errors}", errorMessage);
             throw new ArgumentException(errorMessage, nameof(settings));
         }
     }
 
+    /// <summary>
+    /// Validates the access token.
+    /// </summary>
+    /// <param name="token">The access token to validate.</param>
+    /// <param name="logger">The logger to log any validation errors.</param>
+    /// <exception cref="InvalidOperationException">Thrown if the access token is invalid.</exception>
     public static void ValidateAccessToken(string? token, ILogger logger)
     {
         if (!string.IsNullOrWhiteSpace(token)) return;
 
         logger.LogError("Access Token error: {error}", MissingAccessToken);
-
         throw new InvalidOperationException(MissingAccessToken);
     }
 }
